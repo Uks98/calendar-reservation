e5e6f2e7eca234e36c9506e033c0a5c0e4e61f51
uks98 <rozyfactory@gmail.com>
2022-06-02 19:42:15 +0900

    project2

--- app/src/main/java/com/example/project6_1/MainActivity.kt
+++ app/src/main/java/com/example/project6_1/MainActivity.kt
@@ -1,11 +1,90 @@
 package com.example.project6_1
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.widget.*
+import org.w3c.dom.Text
+import java.time.Month
 
 class MainActivity : AppCompatActivity() {
+    lateinit var chrono : Chronometer
+    lateinit var btnStart : Button
+    lateinit var btnEnd : Button
+    lateinit var rdoCal : RadioButton
+    lateinit var rdoTime : RadioButton
+    lateinit var calView: CalendarView
+    lateinit var tPicker: TimePicker
+    lateinit var tvYear : TextView
+    lateinit var tvMonth: TextView
+    lateinit var tvDay : TextView
+    lateinit var tvHour : TextView
+    lateinit var tvMinute : TextView
+
+    var selectYear : Int = 0
+    var selectMonth : Int = 0
+    var selectDay : Int = 0
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "시간 예약"
+        //버튼
+        btnStart = findViewById(R.id.btnStart)
+        btnEnd = findViewById(R.id.btnEnd)
+        //크로노미터
+        chrono = findViewById<Chronometer>(R.id.chronometer1)
+        //라디오버튼2개
+        rdoCal = findViewById(R.id.rdoCal)
+        rdoTime = findViewById(R.id.rdoTime)
+        //캘린더 뷰
+        calView = findViewById(R.id.calendarView1)
+        //프레임 레이아웃
+        tPicker = findViewById(R.id.timePicker1)
+        calView = findViewById(R.id.calendarView1)
+        //텍스트 뷰 중 연,월,일,시,분,숫자
+        tvYear = findViewById(R.id.tvYear)
+        tvMonth = findViewById(R.id.tvMonth)
+        tvMinute = findViewById(R.id.tvMinute)
+        tvDay = findViewById(R.id.tvDay)
+        tvHour = findViewById(R.id.tvHour)
+
+        //처음에는 두개의 위젯 안보이게 설정
+        tPicker.visibility = View.INVISIBLE
+        calView.visibility = View.INVISIBLE
+
+        rdoCal.setOnClickListener{
+            tPicker.visibility = View.INVISIBLE
+            calView.visibility = View.VISIBLE
+        }
+        rdoTime.setOnClickListener {
+            tPicker.visibility = View.VISIBLE
+            calView.visibility = View.INVISIBLE
+        }
+        btnStart.setOnClickListener {
+            chrono.base = SystemClock.elapsedRealtime()
+            chrono.start()
+            chrono.setTextColor(Color.RED)
+        }
+        btnEnd.setOnClickListener {
+            chrono.stop()
+            chrono.setTextColor(Color.BLUE)
+
+            tvYear.text = Integer.toString(selectYear)
+            tvMonth.text = Integer.toString(selectMonth)
+            tvDay.text = Integer.toString(selectDay)
+            tvHour.text = Integer.toString(tPicker.currentHour)
+            tvMinute.text = Integer.toString(tPicker.currentMinute)
+        }
+        calView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            selectYear = year
+            selectMonth = month +1
+            selectDay = dayOfMonth
+        }
+
     }
 }
